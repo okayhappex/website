fd0d3c82bd2304cda05e5616ec0e85d9d2c21f3b
Loan <110610727+okayhappex@users.noreply.github.com>
2025-11-23 11:40:04 +0100

    TODO: Texte alternatives sur les images

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -33,21 +33,21 @@
 			<div>
 				<div class="grid grid-cols-1 sm:grid-cols-3">
 					<img src="/pf_img/metro.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/fall_1.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/fleur.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/rdm_4.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/fall_2.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/rdm_2.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/voiture.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/rdm_3.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/rdm_1.jpg" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
+					<img src="/pf_img/fall_1.jpg" alt="Photo de la Tour Eiffel depuis l'Avenue des Camoëns (16e)" class="" />
+					<img src="/pf_img/fleur.jpg" alt="Photo d'une fleur" class="" />
+					<img src="/pf_img/rdm_4.jpg" alt="Photo de dominos collés en mots croisés sur un mur, se lisant « love yourself, free yourself, help yourself »" class="" />
+					<img src="/pf_img/fall_2.jpg" alt="Photo d'une rue vide avec des voitures stationnée et deux voitures en circulation" class="" />
+					<img src="/pf_img/rdm_2.jpg" alt="Photo d'une voiture de police devant le musée du Louvre" class="" />
+					<img src="/pf_img/voiture.jpg" alt="Photo d'une voiture en-dessous de la station de métro Bir-Hakeim (ligne 6)" class="" />
+					<img src="/pf_img/rdm_3.jpg" alt="Photo d'un grand immeuble entre deux autres immeubles" class="" />
+					<img src="/pf_img/rdm_1.jpg" alt="Reflet d'une plaque d'immatriculation sous une moto" class="" />
 				</div>
 			</div>
 			<div class="space-y-4 sm:space-y-8">
 				<h3 class="text-white/90 text-lg text-center font-bold sm:text-2xl lg:text-3xl">Quel animal voyez-vous ?</h3>
 				<div class="grid grid-cols-1 sm:grid-cols-2">
-					<img src="/pf_img/moodboard_1.png" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
-					<img src="/pf_img/moodboard_2.png" alt="Photo d'une rame MP05 à la station de métro Bir-Hakeim (ligne 6)" class="" />
+					<img src="/pf_img/moodboard_1.png" alt="Moodboard montrant des images ayant un rapport avec la coccinelle" class="" />
+					<img src="/pf_img/moodboard_2.png" alt="Moodboard montrant des motifs ressemblant à ceux de la coccinelle" class="" />
 				</div>
 			</div>
 		</section>
